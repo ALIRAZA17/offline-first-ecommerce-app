6d39df9eb6bf528298acb94cea182e84226114cc
ALIRAZA17 <ialidar2001@gmail.com>
2025-12-01 17:24:37 +0100

    chore: remove unwanted files

--- lib/data/local/daos/products_dao_di.dart
+++
@@ -1,8 +0,0 @@
-import 'package:injectable/injectable.dart';
-import 'package:offline_ecommerce/data/local/daos/products_dao.dart';
-import 'package:offline_ecommerce/data/local/db/app_database_di.dart';
-
-@LazySingleton()
-class ProductsDaoDI extends ProductsDao {
-  ProductsDaoDI(AppDatabaseProvider provider) : super(provider.db);
-}
